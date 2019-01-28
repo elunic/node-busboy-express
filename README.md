62826e60e16810f0f30797cab2d5f887af38a710
whefter <wh@elunic.com>
2019-01-28 12:52:56 +0100

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,87 @@
+# busboy-express
+
+[![Build Status](https://travis-ci.org/elunic/node-busboy-express.svg?branch=master)](https://travis-ci.org/elunic/node-busboy-express)
+
+An express middleware for `busboy`, with TypeScript typings.
+
+## Usage
+
+**Note**: if you get errors about incompatible definitions of `Express.Request.files`, enable the `skipLibCheck` TypeScript compiler option.
+
+* It is recommened to only include the middleware on routes that actually require it.
+* On routes that have included the middleware, `req.fields` and `req.files` will be key-value objects, with the keys
+  being the fieldnames and the values being arrays of values (for fields) or file descriptor objects (for files).
+
+**Note**: Don't forget to **cleanup** by running `cleanup(req)`. Your route handler doesn't have to wait
+  for its completion.
+
+```javascript
+const busboy = require('busboy-express');
+const express = require('express');
+
+const app = express();
+
+app.post('/upload', busboy({fields: ['allowedfield'], files: ['allowedfile']}), (req, res, next) => {
+  for (const fieldname in req.files) {
+    for (const file of req.files[fieldname]) {
+      moveUploadedFile(file.path, './uploads/' + file.originalFilename);
+    }
+  }
+  
+  for (const fieldname of req.fields) {
+    // process field
+  }
+  
+  busboy.cleanup(req);
+});
+```
+
+
+### Middleware options
+
+The `busboy` middleware takes an `Options` object. This object extends Busboy's own
+`Config` object (the object passed to the `Busboy` constructor).
+
+Documentation for the native `busboy` options object can be found at https://github.com/mscdex/busboy#busboy-methods.
+
+It augments that object with a few options:
+
+```
+{
+  "multipartOnly": true,
+  "uploadPath": os.tmpdir(),
+  "fields": true,
+  "files": []
+}
+```
+
+
+#### `multipartOnly` (boolean)
+
+Handle `multipart/form-data` requests only. This enhances compatiblity if you are using another module
+to handle `urlencoded` requests.
+
+#### `uploadPath` (string)
+
+The path to which files should be uploaded.
+
+#### `fields` (boolean/string[])
+
+An array of fieldnames that should be accepted as fields. No other fields will be processed when populating
+`req.fields`.
+
+Setting this to `true` allows all fields. Limits as defined through the native
+`busboy` options still apply.
+
+#### `files` (boolean/string[])
+
+An array of fieldnames that should be accepted as files. No other files will be processed when populating
+`req.files`.
+
+Setting this to `true` allows all files. Limits as defined through the native
+`busboy` options still apply. Use with caution and consider setting a file count/file size limit.
+
+
+### Debug messages
+
+This module uses `debug` with the `busboy-express` namespace.
---
+++ src/index.ts
@@ -0,0 +1,251 @@
+import * as Busboy from 'busboy';
+import * as Debug from 'debug';
+import { NextFunction, Request, Response } from 'express';
+import * as fs from 'fs-extra';
+import * as os from 'os';
+import * as path from 'path';
+import * as uniqid from 'uniqid';
+import * as util from 'util';
+
+const debug = Debug('busboy-express');
+
+const defaultOptions: Options = {
+  multipartOnly: true,
+  uploadPath: os.tmpdir(),
+  fields: true,
+  files: [],
+};
+
+export interface File {
+  fieldname: string;
+  filename: string;
+  path: string;
+  encoding: string;
+  mimetype: string;
+  size: number;
+}
+export interface Files {
+  [key: string]: File[];
+}
+
+export type Field = unknown;
+export interface Fields {
+  [key: string]: Field[];
+}
+
+export interface Options extends busboy.BusboyConfig {
+  multipartOnly?: boolean;
+  uploadPath?: string;
+  fields?: string[] | boolean;
+  files?: string[] | boolean;
+}
+
+declare global {
+  namespace Express {
+    interface Request {
+      fields: Fields;
+      files: Files;
+    }
+  }
+}
+
+function busboyExpress(mwOptions?: Options) {
+  const options: Options = Object.assign({}, defaultOptions, mwOptions || {});
+
+  return function busboyMiddleware(req: Request, res: Response, next: NextFunction) {
+    try {
+      if (req.method !== 'POST') {
+        next();
+      }
+
+      if (
+        options.multipartOnly &&
+        (!req.headers['content-type'] ||
+          !req.headers['content-type'].includes('multipart/form-data'))
+      ) {
+        next();
+      }
+
+      if (
+        (!options.fields || (options.fields instanceof Array && !options.fields.length)) &&
+        (!options.files || (options.files instanceof Array && !options.files.length))
+      ) {
+        // Nothing to do.
+        next();
+      }
+
+      debug(`${req.method} ${req.url}`);
+
+      req.files = {};
+      req.fields = {};
+
+      const fileWritePromises: Array<Promise<void>> = [];
+
+      const busboy = new Busboy({ headers: req.headers });
+      busboy.on('file', (fieldname, stream, filename, encoding, mimetype) => {
+        if (
+          !options.files ||
+          (options.files instanceof Array && !options.files.includes(fieldname))
+        ) {
+          stream.resume();
+          return;
+        }
+
+        const targetPath = path.join(options.uploadPath || defaultOptions.uploadPath!, uniqid());
+        debug(
+          `${req.method} ${req.url}` +
+            ` FILE [${fieldname}]:` +
+            ` filename: ${filename}, ` +
+            ` encoding: ${encoding}, ` +
+            ` mimetype: ${mimetype}, ` +
+            ` targetPath: ${targetPath}` +
+            ``,
+        );
+
+        fileWritePromises.push(
+          new Promise((resolve, reject) => {
+            try {
+              stream.on('error', err => {
+                debug(
+                  `${req.method} ${req.url}` +
+                    ` READ ERROR: ` +
+                    ` FILE [${fieldname}]:` +
+                    ` filename: ${filename} ` +
+                    ``,
+                  err,
+                );
+
+                resolve();
+              });
+
+              const wStream = fs.createWriteStream(targetPath);
+              wStream.on('error', err => {
+                debug(
+                  `${req.method} ${req.url}` +
+                    ` WRITE ERROR: ` +
+                    ` FILE [${fieldname}]:` +
+                    ` filename: ${filename} ` +
+                    ``,
+                  err,
+                );
+              });
+
+              stream.pipe(
+                wStream,
+                { end: false },
+              );
+              stream.on('end', () => {
+                wStream.end(async () => {
+                  try {
+                    const stats = await fs.stat(targetPath);
+
+                    if (!req.files.hasOwnProperty(fieldname)) {
+                      req.files[fieldname] = [];
+                    }
+
+                    req.files[fieldname].push({
+                      fieldname,
+                      path: targetPath,
+                      size: stats.size,
+                      filename,
+                      encoding,
+                      mimetype,
+                    });
+                    resolve();
+                  } catch (err) {
+                    debug(
+                      `${req.method} ${req.url}` +
+                        ` STAT ERROR: ` +
+                        ` FILE [${fieldname}]:` +
+                        ` filename: ${filename} ` +
+                        ``,
+                      err,
+                    );
+                    resolve();
+                  }
+                });
+              });
+            } catch (err) {
+              debug(
+                `${req.method} ${req.url}` +
+                  ` FILE ERROR: ` +
+                  ` FILE [${fieldname}]:` +
+                  ` filename: ${filename} ` +
+                  ``,
+                err,
+              );
+            }
+          }),
+        );
+      });
+
+      busboy.on('field', (fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) => {
+        if (
+          !options.fields ||
+          (options.fields instanceof Array && !options.fields.includes(fieldname))
+        ) {
+          return;
+        }
+
+        debug(
+          `${req.method} ${req.url}` +
+            ` FIELD [${fieldname}]:` +
+            ` value: ${util.inspect(val)}, ` +
+            ` encoding: ${encoding}, ` +
+            ` mimetype: ${mimetype}, ` +
+            ``,
+        );
+
+        if (!req.fields.hasOwnProperty(fieldname)) {
+          req.fields[fieldname] = [];
+        }
+
+        req.fields[fieldname].push(val);
+      });
+
+      busboy.on('finish', async () => {
+        debug(`${req.method} ${req.url} FINISH, awaiting file writes`);
+        await Promise.all(fileWritePromises);
+        debug(`${req.method} ${req.url} DONE`);
+
+        next();
+      });
+
+      busboy.on('error', (err: Error) => {
+        debug(`${req.method} ${req.url} ERROR`, err);
+
+        next(err);
+      });
+
+      req.pipe(busboy);
+    } catch (err) {
+      next(err);
+    }
+  };
+}
+export { busboyExpress };
+export default busboyExpress;
+
+export async function cleanup(req: Request) {
+  if (req.files) {
+    try {
+      await Promise.all(
+        Object.keys(req.files).map(async fieldname => {
+          await Promise.all(
+            req.files[fieldname].map(async file => {
+              try {
+                if (await fs.pathExists(file.path)) {
+                  await fs.remove(file.path);
+                }
+              } catch (err) {
+                debug(`Failed to cleanup file: ${file.path} (${file.filename}`, err);
+              }
+            }),
+          );
+        }),
+      );
+    } catch (err) {
+      debug(`Error cleaning up uploaded files`, err);
+    }
+  }
+}
